88c5cf0182d8c347ba1b6534878063f6b032d5d2
LioK <73439296+LioK251@users.noreply.github.com>
2021-07-27 01:31:05 +0900

    Update uilibrary.lua

--- uilibrary.lua
+++ uilibrary.lua
@@ -1,14 +1,18 @@
 local library = {}
 
+local toggled = false
+
 function library:CreateWindow(name)
     local Design = Instance.new("ScreenGui")
     local TopMain = Instance.new("ImageLabel")
     local Container = Instance.new("ImageLabel")
     local UIGridLayout = Instance.new("UIGridLayout")
     local Title = Instance.new("TextBox")
+    local ToggleVisible = Instance.new("TextButton")
+    local TextButton_Roundify_5px = Instance.new("ImageLabel")
 
     UIGridLayout.Parent = Container
-    UIGridLayout.CellSize = UDim2.new(0, 350, 0, 25)
+    UIGridLayout.CellSize = UDim2.new(0, 350, 0, -25)
     UIGridLayout.StartCorner = Enum.StartCorner.BottomLeft
 
     Title.Name = "Title"
@@ -19,15 +23,18 @@ function library:CreateWindow(name)
     Title.Position = UDim2.new(0.0228571426, 0, 0, 0)
     Title.Size = UDim2.new(0, 216, 0, 20)
     Title.Font = Enum.Font.GothamBold
-    Title.Text = "Label"
+    Title.Text = name
     Title.TextColor3 = Color3.fromRGB(255, 255, 255)
     Title.TextSize = 20.000
     Title.TextXAlignment = Enum.TextXAlignment.Left
     Title.TextYAlignment = Enum.TextYAlignment.Bottom
-    Title.Text = name
+    Title.ClearTextOnFocus = false
+    Title.Selectable = false
+    Title.TextEditable = false
 
     Design.Name = "Design"
     Design.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+    Design.ResetOnSpawn = false
     
     TopMain.Name = "TopMain"
     TopMain.Parent = Design
@@ -53,9 +60,47 @@ function library:CreateWindow(name)
     Container.SliceCenter = Rect.new(100, 100, 100, 100)
     Container.SliceScale = 0.050
 
+    ToggleVisible.Name = "ToggleVisible"
+    ToggleVisible.Parent = game.ReplicatedStorage
+    ToggleVisible.BackgroundColor3 = Color3.fromRGB(235, 241, 203)
+    ToggleVisible.BackgroundTransparency = 1.000
+    ToggleVisible.BorderSizePixel = 0
+    ToggleVisible.Position = UDim2.new(0.928571463, 0, 0.0508928299, 0)
+    ToggleVisible.Size = UDim2.new(0, 18, 0, 18)
+    ToggleVisible.Font = Enum.Font.GothamSemibold
+    ToggleVisible.Text = ""
+    ToggleVisible.TextColor3 = Color3.fromRGB(255, 255, 255)
+    ToggleVisible.TextSize = 14.000
+    ToggleVisible.MouseButton1Down:Connect(function()
+        if toggled then
+            toggled = false
+            Container:TweenSize(UDim2.new(0, 350,0, 9), "InOut", "Linear", 0.3)
+        else
+            toggled = true
+            Container:TweenSize(UDim2.new(0, 350,0, 350), "InOut", "Linear", 0.3)
+        end
+    end)
+    
+    TextButton_Roundify_5px.Name = "TextButton_Roundify_5px"
+    TextButton_Roundify_5px.Parent = ToggleVisible
+    TextButton_Roundify_5px.Active = true
+    TextButton_Roundify_5px.AnchorPoint = Vector2.new(0.5, 0.5)
+    TextButton_Roundify_5px.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+    TextButton_Roundify_5px.BackgroundTransparency = 1.000
+    TextButton_Roundify_5px.Position = UDim2.new(0.833333373, 0, 0.5, 0)
+    TextButton_Roundify_5px.Selectable = true
+    TextButton_Roundify_5px.Size = UDim2.new(1, 0, 1, 0)
+    TextButton_Roundify_5px.Image = "rbxassetid://3570695787"
+    TextButton_Roundify_5px.ImageColor3 = Color3.fromRGB(235, 241, 203)
+    TextButton_Roundify_5px.ScaleType = Enum.ScaleType.Slice
+    TextButton_Roundify_5px.SliceCenter = Rect.new(100, 100, 100, 100)
+    TextButton_Roundify_5px.SliceScale = 0.050
+
     local EpicLibrary = {}
 
-    function EpicLibrary:CreateButton(text)
+    function EpicLibrary:CreateButton(text, callback)
+        local callback = callback or function() end
+
         local TextButton = Instance.new("TextButton")
 
         TextButton.Parent = Container
@@ -67,6 +112,10 @@ function library:CreateWindow(name)
         TextButton.TextColor3 = Color3.fromRGB(255, 255, 255)
         TextButton.TextSize = 14.000
         TextButton.Text = text
+
+        TextButton.MouseButton1Down:Connect(function()
+            pcall(callback)
+        end)
     end
     return EpicLibrary
 end
